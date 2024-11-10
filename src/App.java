c725c9280ca1dc5f22f63c794c587ac5072ccea9
NinjaFlor <andreiac.lima@outlook.com.br>
2024-11-10 13:50:34 -0300

    Teste de debugging

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Iniciou o programa no metodo main.");
+        a();
+        System.out.println("Finalizou o programa no metodo main.");
     }
+
+    public static void a() {
+        System.out.println("Entrou no metodo a.");
+        b();
+        System.out.println("Finalizou o metodo a.");
+    }
+
+    public static void b() {
+        System.out.println("Entrou no metodo b.");
+        for (int i = 0; i <= 4; i++)
+            System.out.println(i);
+        c();
+        System.out.println("Finalizou o metodo b.");
+    }
+
+    public static void c() {
+        System.out.println("Entrou no metodo c.");
+        Thread.dumpStack();
+        System.out.println("Finalizou o metodo c.");
+    }
+
 }
